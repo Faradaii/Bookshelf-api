f32bcd7aa8725df79ee6b6a10ea8152f3ec55c57
Faradaii B <kdkfrdy@gmail.com>
2024-04-12 09:48:54 +0800

    Chore: update dependencies, init books.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const hapi = require("@hapi/hapi");
-const routes = require('./routes')
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = hapi.server({
     port: 9000,
-    host: "localhost"
+    host: 'localhost',
   });
 
   server.route(routes);
